2e0de90135c7fac15cea1948895e89bf3d19afd5
Amanda Oliveira <amandacoliveira95@gmail.com>
2025-08-25 14:04:40 -0300

    Desfazendo o erro proposital

--- service/transferService.js
+++ service/transferService.js
@@ -4,7 +4,7 @@ const { getUser } = require('./userService');
 function transfer({ remetente, destinatario, valor }) {
   const remetenteUser = getUser(remetente);
   const destinatarioUser = getUser(destinatario);
-  if (!remetenteUser || !destinatarioUser) throw new Error('Usuário, encontrado');
+  if (!remetenteUser || !destinatarioUser) throw new Error('Usuário não encontrado');
   const isFavorecido = remetenteUser.favorecidos.includes(destinatario);
   if (!isFavorecido && valor >= 5000) {
     throw new Error('Transferências acima de R$ 5.000,00 só para favorecidos');
